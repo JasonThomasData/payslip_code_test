fa42667d220da75013e0cd84ebe1e2af5fa6119c
john smith <jason.robert.thomas.123@gmail.com>
2017-06-18 20:29:52 +1000

    Another function to retrieve tax thresholds from the config file

--- app/parse_config_vars.py
+++ app/parse_config_vars.py
@@ -5,16 +5,23 @@ import yaml
 Must be loaded at the start of any process to parse the config.yml
 '''
 
-def get_database_paths():
+def read_config_file():
     with open('config.yml', 'r') as config_file:
         config_data = yaml.load(config_file)
-        database_paths = config_data["data_paths"]
-        return database_paths
+        return config_data
+
+def get_database_paths():
+    config_data = read_config_file()
+    database_paths = config_data["data_paths"]
+    return database_paths
 
 def get_cli_allowed_args():
-    with open('config.yml', 'r') as config_file:
-        config_data = yaml.load(config_file)
-        required_args = config_data["payslips_cli"]["arguments"]["required"]
-        optional_args = config_data["payslips_cli"]["arguments"]["optional"]
-        return required_args, optional_args
+    config_data = read_config_file()
+    required_args = config_data["payslips_cli"]["arguments"]["required"]
+    optional_args = config_data["payslips_cli"]["arguments"]["optional"]
+    return required_args, optional_args
 
+def get_tax_rates():
+    config_data = read_config_file()
+    tax_rates = config_data['tax_rates']
+    return tax_rates
